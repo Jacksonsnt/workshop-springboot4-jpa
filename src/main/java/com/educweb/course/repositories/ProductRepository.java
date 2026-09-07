6b6845ed44329dec8f2deda38492a1448a3d7b6a
Jackson Silva <jacksonsantosdasilvab@gmail.com>
2026-09-07 18:21:48 -0300

    Product

---
+++ src/main/java/com/educweb/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.educweb.course.repositories;
+
+import com.educweb.course.entities.Category;
+import com.educweb.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
